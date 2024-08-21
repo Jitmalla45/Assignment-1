13b969d50ba37c8098d096a3d3df93a1789dbc31
Jitmalla45 <jitmalla45@gmail.com>
2024-08-21 17:06:32 +0530

    Add files via upload

---
+++ Q3a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char vowel1='a',vowel2='e',vowel3='i',vowel4='o',vowel5='u';
+    char capVowel1='A',capVowel2='E',capVowel3='I',capVowel4='O',capVowel5='U';
+    int v21 = vowel2-vowel1, v52 = vowel5-vowel2, cv21 = capVowel2-capVowel1;
+    
+    cout << "vowel2-vowel1 : " << v21 << endl;
+    cout << "vowel5-vowel2 : " << v52 << endl;
+    cout << "capVowel2-capVowel1 : " << cv21 << endl;
+
+	/* All the expressions are valid because the difference between characters 
+   is stored as the difference between their integer ASCII values.
+   The difference in ASCII values between 'a' and 'e' is the same as 
+   the difference between 'A' and 'E'.
+*/ 
+
+}
