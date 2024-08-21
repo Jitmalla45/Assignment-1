50dfeaa6a11316b268ca7d625a33057c2413a509
Jitmalla45 <jitmalla45@gmail.com>
2024-08-21 16:50:23 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace::std;
+int main(){
+		int num;
+		cout<<"Enter a number : ";
+		cin>>num;
+		if(num>=365){
+			cout<<(num/365)<<" years ";
+			num%=365;
+		}
+		if(num>=7){
+			cout<<(num/7)<<" weeks ";
+			num%=7;
+		}
+		cout<<num<<" days"<<endl;
+		return 0;
+}
